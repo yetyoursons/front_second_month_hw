70dc1ae7a1b51d323ae3bb69b3e321422fc7fbee
Aman Kurmangaliev <94127505+yetyoursons@users.noreply.github.com>
2022-04-12 22:38:33 +0600

    Add files via upload

--- HW_2/h2_2.js
+++ HW_2/h2_2.js
@@ -1,7 +1,7 @@
 for (let star = "*"; star.length < 8; star += "*")
     console.log(star)
 
-for (let i = 1; i <= 101; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
         console.log(i, "Fizz");
     } else if (i % 5 === 0) {
