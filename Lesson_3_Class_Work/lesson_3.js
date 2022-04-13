204d898ca28e4ae44bf2c0d7fe5ad0e4a4ec30df
Aman Kurmangaliev <94127505+yetyoursons@users.noreply.github.com>
2022-04-13 19:06:07 +0600

    Add files via upload

---
+++ Lesson_3_Class_Work/lesson_3.js
@@ -0,0 +1,29 @@
+
+// function numbers(number1,number2) {
+//     if (number1<number2){
+//         console.log("Первое число больше!")
+//     } else{
+//         console.log("Второе число больше!")
+//     }
+// }
+// const number = {
+//     number1: 5,
+//     number2: 2
+// }
+// numbers(1,2);
+//
+
+function twoMass(arr1, arr2) {
+    if (arr1.length < arr2.length) {
+        console.log("Первый массив больше!")
+    } else {
+        console.log("Второй массив больше!")
+    }
+}
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+twoMass(1, 2);
+
+
