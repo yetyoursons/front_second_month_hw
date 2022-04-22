4c19b2623dfbd34d1fe62439dd4507f31a88ba63
Aman Kurmangaliev <94127505+yetyoursons@users.noreply.github.com>
2022-04-23 00:23:11 +0600

    Add files via upload

--- HW_5/main.js
+++ HW_5/main.js
@@ -1,20 +1,37 @@
-const increase = document.getElementById('plusValue')
-const decrease = document.getElementById('minusValue')
-const score = document.getElementById('score')
-const reset = document.getElementById('reset')
-let value = 0;
-increase.onclick = () => {
-    value++
-    score.innerText = value;
-}
-decrease.onclick = () => {
-    value--
-    if ( value < 1){
-        value = 0
-    }
-    score.innerText = value;
-}
-reset.onclick = () => {
-    value = 0;
-    score.innerHTML = value;
+// const yellow = document.getElementById('yellow')
+// yellow.addEventListener('click', function onClick(event) {
+//     yellow.style.backgroundColor = 'yellow'
+//     alert('Get Ready!')
+// })
+//
+// const red = document.getElementById('red')
+//
+// red.addEventListener('click', function onClick(event) {
+//     red.style.backgroundColor = 'red'
+//     alert('STOP!')
+// })
+//
+// const green = document.getElementById('green')
+//
+// green.addEventListener('click', function onClick(event) {
+// green.style.backgroundColor = 'green'
+//     alert('GO!')
+// })
+// Он работает с первым закоментированым html
+
+let indicator = prompt("Выберите цвет: зеленый, желтый, красный!");
+switch (indicator) {
+    case "красный":
+        document.getElementById('red-indicator').style.backgroundColor = 'red'
+        document.getElementById('red-indicator').innerText = 'STOP!'
+        alert('Stop!')
+        break;
+    case "желтый":
+        document.getElementById('yellow-indicator').style.backgroundColor = 'yellow'
+        document.getElementById('yellow-indicator').innerText = 'Get Ready!'
+        break;
+    case "зеленый":
+        document.getElementById('green-indicator').style.backgroundColor = 'green'
+        document.getElementById('green-indicator').innerText = 'GO!'
+        break;
 }
